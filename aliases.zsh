3e4a640fbd2df396e636b6d67519011b1f91a295
Stefano Torresi <stefano@torresi.io>
2019-07-02 13:28:14 +0200

    add docker-ip alias

--- aliases.zsh
+++ aliases.zsh
@@ -3,5 +3,6 @@ alias ll='ls -lAh'
 alias la='ls -laF'
 alias vi='vim'
 alias php-debug='php -dzend_extension=xdebug.so'
+alias docker-ip="docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'"
 unalias _
 unalias c
